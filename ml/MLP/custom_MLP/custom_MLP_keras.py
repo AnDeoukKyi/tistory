82cd5047b86b3cc82e075d9e9601e9acc5bcdfc7
AnDeoukKyi <abdgf3@naver.com>
2021-08-20 17:53:57 +0900

    a

--- ml/MLP/custom_MLP/custom_MLP_keras.py
+++ ml/MLP/custom_MLP/custom_MLP_keras.py
@@ -35,6 +35,7 @@ two = np.array([    1, 1, 1
                    ,1, 1, 1], dtype="uint8")# * 255
 # print("zero\n", zero)
 # print("one\n", one)
+# print("two\n", two)
 
 train_x = np.array([ [1, 1, 0,1, 0, 1,1, 0, 1,1, 0, 1,1, 1, 1]# 0
                     ,[0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1]# 0
@@ -44,8 +45,12 @@ train_x = np.array([ [1, 1, 0,1, 0, 1,1, 0, 1,1, 0, 1,1, 1, 1]# 0
                     ,[0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1]# 1
                     ,[0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1]# 1
                     ,[0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1]# 1
+                    ,[1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]# 2
+                    ,[0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1]# 2
+                    ,[1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1]# 2
+                    ,[1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1]# 2
                     ], dtype="uint8")# * 255
-train_y = np.array([0, 0, 0, 0, 1, 1, 1, 1], dtype="uint8")# * 255
+train_y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], dtype="uint8")# * 255
 
 # print(train_x)
 # print(train_y)
@@ -54,30 +59,39 @@ train_y = np.array([0, 0, 0, 0, 1, 1, 1, 1], dtype="uint8")# * 255
 # input - hidden layer
 w1 = np.random.randn(15)
 # print("weight1\n", w1)
-b1 = np.random.randn(6)
+
+b1 = np.random.randn(10)
 # print("bias1\n", b1)
 
-
 # hidden - output layer
-w2 = np.random.randn(6)
+w2 = np.random.randn(10)
 # print("weight2\n", w2)
-b2 = np.random.randn(1)
+b2 = np.random.randn(4)
 # print("bias2\n", b2)
 
-epoch = 1000
+# hidden - output layer
+w3 = np.random.randn(4)
+# print("weight2\n", w2)
+b3 = np.random.randn(1)
+# print("bias2\n", b2)
+
+epoch = 2000
 learnRate = 0.7
 mse = []
 Error = None
+
 for index_epoch in range(epoch):
     print(index_epoch+1, "번째 학습입니다.", index_epoch+1 , "/", epoch)
-    err = []
+
     for index_train in range(len(train_x)):
         # FeedForword(순전파)
 
         # input to hidden Layer
         i2hLayer = sigmoid(weighted_sum(train_x[index_train], w1, b1))
+        # input to hidden Layer
+        i2hLayer1 = sigmoid(weighted_sum(i2hLayer, w2, b2))
         # hidden to output Layer
-        h2oLayer = sigmoid(weighted_sum(i2hLayer, w2, b2))
+        h2oLayer = sigmoid(weighted_sum(i2hLayer1, w3, b3))
 
         #error
         Error = ((train_y[index_train] - h2oLayer)**2)/2
@@ -85,18 +99,22 @@ for index_epoch in range(epoch):
 
         #Back-Propagation(역전파)
         # hidden to output Layer
-        a2 = -(train_y[index_train] - h2oLayer) * h2oLayer * h2oLayer * (1 - h2oLayer)
-        b2 = b2 - learnRate * a2
-        w2 = w2 - (learnRate * a2 * i2hLayer)
+        a3 = -(h2oLayer - train_y[index_train]) * h2oLayer * (1 - h2oLayer)
+        b3 = b3 + learnRate * a3
+        w3 = w3 + (learnRate * a3 * i2hLayer1)
+
+        a2 = np.sum(a3 * i2hLayer1 * (1 - i2hLayer1))
+        b2 = b2 + learnRate * a2
+        w2 = w2 + (learnRate * a2 * i2hLayer)
 
         # input to hidden Layer
         a1 = np.sum(a2 * i2hLayer * (1 - i2hLayer))
-        b1 = b1 - learnRate * a1
-        w1 = w1 - (learnRate * a1 * train_x[index_train])
-        err.append(Error)
-        print("mse", Error)
+        b1 = b1 + learnRate * a1
+        w1 = w1 + (learnRate * a1 * train_x[index_train])
+        # print("mse", Error)
     #학습완료
-    print("mse", sum(err))
+    print("mse", Error)
+    # mse.append(np.mean(Error ** 2))
 
 
 
@@ -108,12 +126,12 @@ for index_epoch in range(epoch):
 
 
 
-test = [zero, one]
-for index in range(len(test)):
+test = [zero, one, two]
+for input in test:
     # input to hidden Layer
-    i2hLayer = sigmoid(weighted_sum(test[index], w1, b1))
+    i2hLayer = sigmoid(weighted_sum(train_x[index_train], w1, b1))
 
     # hidden to output Layer
     h2oLayer = sigmoid(weighted_sum(i2hLayer, w2, b2))
 
-    print(h2oLayer)
+    print(softmax(h2oLayer))
