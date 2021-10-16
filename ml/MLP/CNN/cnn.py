ce45da4ffe5786aec9900ce8c1a4fb83edd3779c
AnDeoukKyi <44719109+AnDeoukKyi@users.noreply.github.com>
2021-10-16 16:35:19 +0900

    a

--- ml/MLP/CNN/cnn.py
+++ ml/MLP/CNN/cnn.py
@@ -1,37 +1,30 @@
 import tensorflow as tf
-from tensorflow.keras import datasets
 
-# 1. MNIST 데이터셋 임포트
-mnist = datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+from data_mnist import mnist2Data
 
-# 2. 데이터 전처리
+x_train, y_train, x_test, y_test = mnist2Data()
+
 x_train, x_test = x_train/255.0, x_test/255.0
+#CNN의 경우 input shape에 채널까지 들어가야함(흑백 1채널, rgb 3채널)
+x_train = x_train.reshape(len(x_train), x_train.shape[1], x_train.shape[2], 1)
+x_test = x_test.reshape(len(x_test), x_test.shape[1], x_test.shape[2], 1)
 
 model = tf.keras.models.Sequential()
-
-
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 3)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(x_train.shape[1], x_train.shape[2], 1)))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-
-
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-
-
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-
-
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.Dense(2, activation='softmax'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 model.summary()
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=20)
+model.fit(x_train, y_train, epochs=5)
 
 test_loss, test_acc = model.evaluate(x_train, y_train, verbose=2)
 print(test_acc)
